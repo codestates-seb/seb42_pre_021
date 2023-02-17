e6dc268335a9b35ba00db51e06b56fa29addd3a1
Rose Jang <jangmi749@gmail.com>
2023-02-17 14:44:07 +0900

    :lipstick: design : Header, Main 영역 레이아웃 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div className="App"></div>;
-}
+import styled from 'styled-components';
+import Header from './containers/Header';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <MainContainer></MainContainer>
+    </div>
+  );
+};
+
+const MainContainer = styled.main`
+  width: 100%;
+  height: 200%;
+  padding-top: 3rem;
+`;
 
 export default App;
--- client/src/containers/Header.js
+++ client/src/containers/Header.js
@@ -4,10 +4,13 @@ const Header = () => {
   return <HeaderContainer>Header</HeaderContainer>;
 };
 
-const HeaderContainer = styled.div`
+const HeaderContainer = styled.header`
   background-color: red;
   width: 100%;
   height: 3rem;
+  position: fixed;
+  top: 0;
+  left: 0;
 `;
 
 export default Header;
