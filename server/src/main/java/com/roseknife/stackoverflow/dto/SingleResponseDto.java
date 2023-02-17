b051937edfd0c4bac894eb5a2e4112d63581cd70
JoeGukseon <zipcks1381@gmail.com>
2023-02-17 18:00:42 +0900

    :sparkles: feat : 질문 등록 API 추가 SingleResponseDto 공통 Class 추가

---
+++ server/src/main/java/com/roseknife/stackoverflow/dto/SingleResponseDto.java
@@ -0,0 +1,10 @@
+package com.roseknife.stackoverflow.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class SingleResponseDto<T> {
+    private T data;
+}
--- server/src/main/java/com/roseknife/stackoverflow/utils/UriCreator.java
+++ server/src/main/java/com/roseknife/stackoverflow/utils/UriCreator.java
@@ -7,9 +7,9 @@ import java.net.URI;
 public class UriCreator {
     public static URI createUri(String defaultUrl, long resourceId) {
         return UriComponentsBuilder
-            .newInstance()
-            .path(defaultUrl + "/{resource-id}")
-            .buildAndExpand(resourceId)
-            .toUri();
+                .newInstance()
+                .path(defaultUrl + "/{resource-id}")
+                .buildAndExpand(resourceId)
+                .toUri();
     }
 }
