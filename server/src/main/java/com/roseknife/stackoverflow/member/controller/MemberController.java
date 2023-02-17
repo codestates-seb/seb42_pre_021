be246356fcb5b4be5424e19a556a060556dfe315
seungchul <seungchul0118@gmail.com>
2023-02-17 16:45:37 +0900

    :sparkles: feat : 편의 기능(UriCreator) 추가

--- server/src/main/java/com/roseknife/stackoverflow/member/controller/MemberController.java
+++ server/src/main/java/com/roseknife/stackoverflow/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.roseknife.stackoverflow.member.dto.MemberDto;
 import com.roseknife.stackoverflow.member.entity.Member;
 import com.roseknife.stackoverflow.member.mapper.MemberMapper;
 import com.roseknife.stackoverflow.member.service.MemberService;
+import com.roseknife.stackoverflow.utils.UriCreator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -20,22 +21,18 @@ import java.net.URI;
 @RestController
 @Validated
 @RequestMapping("/members")
-@Slf4j
 @RequiredArgsConstructor
 public class MemberController {
+    private final static String MEMBER_DEFAULT_RUL = "/members";
     private final MemberService memberService;
     private final MemberMapper mapper;
 
     @PostMapping()
     public ResponseEntity postMember(@Valid @RequestBody MemberDto.Post requestBody) {
         Member member = mapper.memberPostToMember(requestBody);
-        memberService.createMember(member);
 
-        log.info("create member");
-        URI location = UriComponentsBuilder
-            .newInstance()
-            .path("/members/{member-id}")
-            .buildAndExpand(member.getMemberId()).toUri();
+        Member createdMember = memberService.createMember(member);
+        URI location = UriCreator.createUri(MEMBER_DEFAULT_RUL, createdMember.getMemberId());
 
         return ResponseEntity.created(location).build();
     }
