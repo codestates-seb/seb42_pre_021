788acf841168207c9f06f6bfdf5e6008fef61d51
Rose Jang <jangmi749@gmail.com>
2023-02-17 14:56:00 +0900

    :sparkles: feat : Router 셋팅 적용

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,14 @@
+import AnswerEdit from 'pages/AnswerEdit';
+import Login from 'pages/Login';
+import MyPage from 'pages/MyPage';
+import MyPageEdit from 'pages/MyPageEdit';
+import QuestionAdd from 'pages/QuestionAdd';
+import QuestionDetail from 'pages/QuestionDetail';
+import QuestionEdit from 'pages/QuestionEdit';
+import Questions from 'pages/Questions';
+import SignUp from 'pages/SignUp';
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 import styled from 'styled-components';
 import Header from './containers/Header';
 
@@ -5,7 +16,19 @@ const App = () => {
   return (
     <div className="App">
       <Header />
-      <MainContainer></MainContainer>
+      <MainContainer>
+        <Routes>
+          <Route path="/" element={<Questions />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/:question-id" element={<QuestionDetail />} />
+          <Route path="/:question-id/edit" element={<QuestionEdit />} />
+          <Route path="/add" element={<QuestionAdd />} />
+          <Route path="/:answer-id/edit" element={<AnswerEdit />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/mypage/edit" element={<MyPageEdit />} />
+        </Routes>
+      </MainContainer>
     </div>
   );
 };
