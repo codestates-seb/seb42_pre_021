f852e62eb705cacf346951372fb434e913a98aa7
sugyeonghh <sugyeong.py@gmail.com>
2023-02-20 17:37:24 +0900

    :sparkles: feat : 질문 북마크 수정 API 구현

--- server/src/main/java/com/roseknife/stackoverflow/question/entity/Question.java
+++ server/src/main/java/com/roseknife/stackoverflow/question/entity/Question.java
@@ -3,6 +3,7 @@ package com.roseknife.stackoverflow.question.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.roseknife.stackoverflow.answer.entity.Answer;
 import com.roseknife.stackoverflow.audit.Auditable;
+import com.roseknife.stackoverflow.bookmark.entity.QuestionBookmark;
 import com.roseknife.stackoverflow.member.entity.Member;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @NoArgsConstructor
@@ -48,4 +50,14 @@ public class Question extends Auditable {
 
 //    @OneToMany(mappedBy = "question")
 //    private List<QuestionVote> questionVotes;
+
+    @OneToOne(mappedBy = "question", cascade = CascadeType.ALL)
+    private QuestionBookmark questionBookmark;
+
+    public void setQuestionBookmark(QuestionBookmark questionBookmark) {
+        this.questionBookmark = questionBookmark;
+        if (questionBookmark.getQuestion() != this) {
+            questionBookmark.setQuestion(this);
+        }
+    }
 }
--- server/src/main/java/com/roseknife/stackoverflow/question/service/RealQuestionService.java
+++ server/src/main/java/com/roseknife/stackoverflow/question/service/RealQuestionService.java
@@ -1,5 +1,6 @@
 package com.roseknife.stackoverflow.question.service;
 
+import com.roseknife.stackoverflow.bookmark.entity.QuestionBookmark;
 import com.roseknife.stackoverflow.exception.BusinessLogicException;
 import com.roseknife.stackoverflow.exception.ExceptionCode;
 import com.roseknife.stackoverflow.question.dto.QuestionDto;
@@ -28,6 +29,7 @@ public class RealQuestionService implements QuestionService{
     }
 
     public Question createQuestion(Question question) {
+        question.setQuestionBookmark(new QuestionBookmark());
         Question savedQuestion = questionRepository.save(question);
         return savedQuestion;
     }
