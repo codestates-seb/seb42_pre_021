8d4c91c13815c66299164b803ad52c9a54b8ce25
sleepyjoyy <sleepyjoyy@gmail.com>
2023-02-20 14:37:18 +0900

    :lipstick: design : 상단 공지 컴포넌트 구현

--- client/src/pages/QuestionAdd.js
+++ client/src/pages/QuestionAdd.js
@@ -1,6 +1,14 @@
 import styled from 'styled-components';
-// import ask_background from 'assets/ask_background';
 import TextEditor from 'components/Editor';
+import { Link } from 'react-router-dom';
+
+const stepList = [
+  'Summarize your problem in a one-line title.',
+  'Describe your problem in more detail.',
+  'Describe what you tried and what you expected to happen.',
+  'Add “tags” which help surface your question to members of the community.',
+  'Review your question and post it to the site.',
+];
 
 const QuestionAdd = () => {
   return (
@@ -11,6 +19,20 @@ const QuestionAdd = () => {
             <h1>Ask a public qeustion</h1>
           </BackgroundWrapper>
         </TipWrapper>
+        <NoticeContainer>
+          <h2>Writing a good question</h2>
+          <p>
+            You’re ready to <Linkto>ask</Linkto> a <Linkto>programming-related question</Linkto> and
+            this form will help guide you through the process.
+            <br />
+            Looking to ask a non-programming question? See <Linkto>the topics here</Linkto> to find
+            a relevant site.
+          </p>
+          <h5>Steps</h5>
+          {stepList.map((step, idx) => {
+            return <li key={idx}>{step}</li>;
+          })}
+        </NoticeContainer>
         <TextEditor />
       </Wrapper>
     </Container>
@@ -33,26 +55,59 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  background-color: #f1f2f3;
 `;
 
 const TipWrapper = styled.div`
+  width: 100%;
   display: flex;
   margin-bottom: 2rem;
+  padding: 1rem;
+
   h1 {
     height: 130px;
     flex-grow: 1;
-    overflow: visible;
   }
 `;
 
 const BackgroundWrapper = styled.div`
-  width: 100vh;
   @media screen and (min-width: 1050px) {
     background-image: url('https://cdn.sstatic.net/Img/ask/background.svg?v=2e9a8205b368');
     height: 130px;
     background-position: right bottom;
     background-repeat: no-repeat;
     flex-grow: 1;
+  }
+`;
+
+const NoticeContainer = styled.div`
+  width: 100%;
+  background-color: #ebf4fb;
+  border: 1px solid #a6ceed;
+  border-radius: 0.2rem;
+  padding: 2rem;
+
+  p {
+    margin-top: 1rem;
+    font-size: 0.95rem;
+  }
+
+  h5 {
+    margin: 1rem 0 0.3rem 0;
+  }
+
+  li {
+    font-size: 0.8rem;
+  }
+`;
+
+const Linkto = styled(Link)`
+  color: #3b95ff;
+  font-size: 0.9rem;
+  text-decoration: none;
+  &:hover,
+  &:focus {
+    color: #80bbff;
   }
 `;
 
