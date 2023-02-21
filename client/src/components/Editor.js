774b71d93ef7f5212b1674a73a516a2d097af373
Rose Jang <jangmi749@gmail.com>
2023-02-21 12:30:00 +0900

    :sparkles: feat : 마크다운 에디터 설정 변경

--- client/src/components/Editor.js
+++ client/src/components/Editor.js
@@ -3,34 +3,23 @@ import { Editor } from '@toast-ui/react-editor';
 
 const TextEditor = ({ editorRef, editorValue, editorHeight }) => {
   return (
-    <>
-      <Editor
-        ref={editorRef} // DOM 선택용 useRef
-        placeholder={editorPlaceholder}
-        previewStyle="vertical" // 미리보기 스타일 지정
-        height={editorHeight} // 에디터 창 높이
-        initialEditType="markdown"
-        useCommandShortcut={false}
-      ></Editor>
-
-      <Editor
-        ref={editorRef} // DOM 선택용 useRef
-        placeholder="please write here..."
-        // previewStyle="vertical" // 미리보기 스타일 지정
-        height={editorHeight} // 에디터 창 높이
-        initialEditType="markdown"
-        useCommandShortcut={false}
-        toolbarItems={[
-          ['heading', 'bold', 'italic', 'strike'],
-          ['hr', 'quote'],
-          ['ul', 'ol', 'task'],
-          ['table', 'link'],
-          ['code', 'codeblock'],
-        ]}
-        initialValue={editorValue}
-        hideModeSwitch={true}
-      ></Editor>
-    </>
+    <Editor
+      ref={editorRef} // DOM 선택용 useRef
+      placeholder="please write here..."
+      // previewStyle="vertical" // 미리보기 스타일 지정
+      height={editorHeight} // 에디터 창 높이
+      initialEditType="markdown"
+      useCommandShortcut={false}
+      toolbarItems={[
+        ['heading', 'bold', 'italic', 'strike'],
+        ['hr', 'quote'],
+        ['ul', 'ol', 'task'],
+        ['table', 'link'],
+        ['code', 'codeblock'],
+      ]}
+      initialValue={editorValue}
+      hideModeSwitch={true}
+    ></Editor>
   );
 };
 
