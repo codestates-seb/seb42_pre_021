18f44ae454e94875834d665649e24090ff897f1f
Diiiiiikey <chest6654@gmail.com>
2023-02-22 20:32:00 +0900

    :recycle: refactor : MyProfileList 에서 MyProfileInput 분리

--- client/src/components/MyProfileList.js
+++ client/src/components/MyProfileList.js
@@ -1,61 +1,26 @@
 import styled from 'styled-components';
-import { FaMapMarkerAlt } from 'react-icons/fa';
 import TextEditor from 'components/Editor';
+import MyProfileInput from './MyProfileInput';
 
-const MyProfileList = ({ username, location, title, aboutme, isEdit }) => {
+const MyProfileList = ({ username, location, title, aboutme, editorRef, isEdit }) => {
   return (
     <InfoMain>
       <ul>
         <li>
-          <label htmlFor="name" className="inputTitle">
-            Display name
-          </label>
-          {isEdit ? (
-            <div className="inputBox">
-              <input id="name" type="text" autoComplete="off" />
-            </div>
-          ) : (
-            <span>{username}</span>
-          )}
+          <MyProfileInput label={'Display name'} value={username} isEdit={isEdit} id={'name'} />
         </li>
         <li>
-          <label htmlFor="location" className="inputTitle">
-            Location
-          </label>
-          {isEdit ? (
-            <div className="inputBox">
-              <input id="location" type="text" autoComplete="off" />
-            </div>
-          ) : (
-            <span>
-              <FaMapMarkerAlt />
-              {location}
-            </span>
-          )}
+          <MyProfileInput label={'Location'} value={location} isEdit={isEdit} id={'location'} />
         </li>
         <li>
-          <label htmlFor="title" className="inputTitle">
-            Title
-          </label>
-          {isEdit ? (
-            <div className="inputBox">
-              <input
-                id="title"
-                type="text"
-                placeholder="No title has been set"
-                autoComplete="off"
-              />
-            </div>
-          ) : (
-            <span>{title}</span>
-          )}
+          <MyProfileInput label={'Title'} value={title} isEdit={isEdit} id={'title'} />
         </li>
         <li>
           <div className="inputTitle">About me</div>
           {isEdit ? (
-            <TextEditor editorValue={' '} editorHeight={'10rem'} />
+            <TextEditor editorRef={editorRef} editorValue={' '} editorHeight={'10rem'} />
           ) : (
-            <AboutMe>{aboutme}</AboutMe>
+            <span>{aboutme}</span>
           )}
         </li>
       </ul>
@@ -73,9 +38,14 @@ const InfoMain = styled.main`
       margin: 0.5rem;
       font-size: 0.9rem;
       font-weight: bold;
-      > .inputTitle {
+      > div {
         margin-bottom: 0.2rem;
+
+        @media screen and (max-width: 640px) {
+          width: 100%;
+        }
       }
+
       > span {
         display: flex;
         align-items: center;
@@ -86,34 +56,8 @@ const InfoMain = styled.main`
         white-space: normal;
         color: #777;
       }
-
-      > .inputBox {
-        display: flex;
-        width: 50%;
-        padding: 0.3rem;
-        border-radius: 4px;
-        border: 1px solid #bbc0c4;
-        &:focus-within {
-          border: 1px solid gray;
-        }
-
-        input {
-          width: 100%;
-          margin: 0.1rem;
-          border: none;
-          font-size: 0.8rem;
-          &:focus {
-            outline: none;
-          }
-        }
-        @media screen and (max-width: 640px) {
-          width: 100%;
-        }
-      }
     }
   }
 `;
-
-const AboutMe = styled.span``;
 
 export default MyProfileList;
