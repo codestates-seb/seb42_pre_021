6974e353ec5a7477e54b6b2ab4ae23402c415d29
Rose Jang <jangmi749@gmail.com>
2023-02-22 21:27:13 +0900

    :lipstick: design : height, max-height 설정

--- client/src/containers/Container.js
+++ client/src/containers/Container.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  border: 2px solid red;
   width: 1280px;
-  height: 100%;
+  height: fit-content;
+  min-height: calc(100vh - 3rem);
   padding-left: 11rem;
   @media screen and (max-width: 640px) {
     padding-left: 0;
