ef557c03bb918d2568c1fa616ee8714b6eb8dd15
Rose Jang <jangmi749@gmail.com>
2023-02-22 17:14:55 +0900

    :sparkles: feat : 페이지 이동 시 최상단으로 스크롤

--- client/src/utils/ScrollTop.js
+++ client/src/utils/ScrollTop.js
@@ -6,8 +6,6 @@ export default function ScrollToTop() {
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    console.log(pathname);
-    console.log(window.scrollY);
   }, [pathname]);
 
   return null;
