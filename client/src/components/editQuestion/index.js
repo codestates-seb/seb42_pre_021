422fbc137563ca74f82ebdbd4aeaba80b0945312
Rose Jang <jangmi749@gmail.com>
2023-02-23 13:21:04 +0900

    :truck: rename : index.js 파일로 컴포넌트 한번에 export

---
+++ client/src/components/editQuestion/index.js
@@ -0,0 +1,8 @@
+import BodyEdit from './BodyEdit';
+import HowToEdit from './HowToEdit';
+import HowToFormat from './HowToFormat';
+import HowToTag from './HowToTag';
+import TagEdit from './TagEdit';
+import TitleEdit from './TitleEdit';
+
+export { BodyEdit, HowToEdit, HowToFormat, HowToTag, TagEdit, TitleEdit };
