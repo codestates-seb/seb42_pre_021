bb9b5bec8474ebe8b51254483a345de48ae379f9
Rose Jang <jangmi749@gmail.com>
2023-02-23 15:59:15 +0900

    :clown_face: other : index.js로 컴포넌트 모아서 export

--- client/src/components/edit/index.js
+++ client/src/components/edit/index.js
@@ -4,5 +4,7 @@ import HowToFormat from './HowToFormat';
 import HowToTag from './HowToTag';
 import TagEdit from './TagEdit';
 import TitleEdit from './TitleEdit';
+import CancelButton from './CancelButton';
+import TopNotice from './TopNotice';
 
-export { BodyEdit, HowToEdit, HowToFormat, HowToTag, TagEdit, TitleEdit };
+export { BodyEdit, HowToEdit, HowToFormat, HowToTag, TagEdit, TitleEdit, CancelButton, TopNotice };
