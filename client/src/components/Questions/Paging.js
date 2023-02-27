7061aa88511637b51d74254d35bd6ee8fac90968
Rose Jang <jangmi749@gmail.com>
2023-02-27 19:59:55 +0900

    :recycle: refactor : 정렬 옵션 선택적으로 보이게 변경

--- client/src/components/Questions/Paging.js
+++ client/src/components/Questions/Paging.js
@@ -22,32 +22,34 @@ const Paging = ({ size, page, setPage, setSize, total }) => {
         nextPageText={'Next'}
         onChange={handlePageChange}
       />
-      <PagePer>
-        <ul>
-          <li
-            role="presentation"
-            className={size === 10 ? 'current_size' : null}
-            onClick={() => handleClickSize(10)}
-          >
-            10
-          </li>
-          <li
-            role="presentation"
-            className={size === 15 ? 'current_size' : null}
-            onClick={() => handleClickSize(15)}
-          >
-            15
-          </li>
-          <li
-            role="presentation"
-            className={size === 20 ? 'current_size' : null}
-            onClick={() => handleClickSize(20)}
-          >
-            20
-          </li>
-        </ul>
-        <p>per page</p>
-      </PagePer>
+      {setSize && (
+        <PagePer>
+          <ul>
+            <li
+              role="presentation"
+              className={size === 10 ? 'current_size' : null}
+              onClick={() => handleClickSize(10)}
+            >
+              10
+            </li>
+            <li
+              role="presentation"
+              className={size === 15 ? 'current_size' : null}
+              onClick={() => handleClickSize(15)}
+            >
+              15
+            </li>
+            <li
+              role="presentation"
+              className={size === 20 ? 'current_size' : null}
+              onClick={() => handleClickSize(20)}
+            >
+              20
+            </li>
+          </ul>
+          <p>per page</p>
+        </PagePer>
+      )}
     </PagingWrapper>
   );
 };
