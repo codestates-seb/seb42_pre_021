a15573984cff10d538ebe1acaa597ccc0d9d0458
Diiiiiikey <chest6654@gmail.com>
2023-02-27 10:48:55 +0900

    :recycle: refactor : customAsios 적용

--- client/src/features/userService.js
+++ client/src/features/userService.js
@@ -1,51 +1,29 @@
-import axios from 'axios';
+import customAxios from 'api/baseURL';
 
-// const URL = 'https://975c-59-10-231-15.jp.ngrok.io/';
-const URL = 'http://localhost:3001/';
-// const URL = 'https://d1ad-1-237-37-135.jp.ngrok.io/';
-// const URL = process.env.REACT_APP_API_URL
-
-// info 요청
 // `${URL}members?memberId=${id}`
-
-axios.defaults.headers.common['Content-Type'] = 'application/json';
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-
 const userInfo = async id => {
-  const res = await axios({
-    url: `${URL}members/${id}`,
-    method: 'get',
-    withCredentials: true,
-  });
-
+  const res = await customAxios.get(`/members/${id}`);
   return res.data;
 };
 
-// info edit 요청
+const users = async () => {
+  const res = await customAxios.get(`/members`);
+  return res.data;
+};
+
 const userInfoEdit = async (userData, id) => {
-  // 넘어오기는 잘 넘어옴
-  console.log(userData, id);
-  const res = await axios({
-    url: `${URL}members/${id}`, // `${URL}members?memberId=${id}`
-    method: 'patch',
-    data: userData,
-    withCredentials: true,
-  });
+  const res = await customAxios.patch(`/members/${id}`, userData);
   return res.data;
 };
 
-// 회원 삭제 요청
 const userDelete = async id => {
-  await axios({
-    url: `${URL}members/${id}`, // `${URL}members?memberId=${id}`
-    method: 'delete',
-    withCredentials: true,
-  });
+  await customAxios.delete(`/members/${id}`);
   localStorage.clear();
 };
 
 const userService = {
   userInfo,
+  users,
   userInfoEdit,
   userDelete,
 };
