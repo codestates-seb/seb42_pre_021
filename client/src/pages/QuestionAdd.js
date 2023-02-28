a5849d629bc1d2e94d5409c42bb920178729796f
Rose Jang <jangmi749@gmail.com>
2023-02-28 09:58:53 +0900

    :lipstick: feat : 반응형 CSS 수정

--- client/src/pages/QuestionAdd.js
+++ client/src/pages/QuestionAdd.js
@@ -198,12 +198,15 @@ const Container = styled.div`
 
 const Wrapper = styled.div`
   padding: 1rem 0 2rem 0;
-  width: 80%;
+  width: 1280px;
   height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-
+  @media screen and (max-width: 1279px) {
+    width: 100%;
+    padding: 1rem 1.5rem;
+  }
   @media screen and (max-width: 1023px) {
     width: 100%;
   }
@@ -228,6 +231,7 @@ const HeaderTitleContainer = styled.div`
   display: flex;
   align-items: center;
 `;
+
 const HeaderBackground = styled.div`
   background-image: url('https://cdn.sstatic.net/Img/ask/background.svg?v=2e9a8205b368');
   display: flex;
@@ -273,8 +277,11 @@ const NoticeContainer = styled.div`
   li {
     font-size: 0.8rem;
   }
+  @media screen and (max-width: 1024px) {
+    width: 100%;
+  }
   @media screen and (max-width: 767px) {
-    width: 70%;
+    /* width: 70%; */
   }
 `;
 
@@ -292,7 +299,7 @@ const TitleContainer = styled.div`
   position: relative;
   min-width: fit-content;
   width: 1024px;
-  height: 10rem;
+  /* height: 10rem; */
   background-color: #ffffff;
   border: 1px solid #e3e6e8;
   display: flex;
@@ -304,6 +311,9 @@ const TitleContainer = styled.div`
   > span {
     font-size: 0.8rem;
     margin-bottom: 0.5rem;
+  }
+  @media screen and (max-width: 1024px) {
+    width: 100%;
   }
 `;
 
@@ -321,13 +331,33 @@ const Input = styled.input`
   border-style: none;
   border-radius: 0.2rem;
   border: 1px solid #babfc4;
+  :focus {
+    border: 1px solid rgba(0, 103, 194, 0.4);
+    box-shadow: 0 0 0 4px rgba(144, 203, 255, 0.4);
+    outline: none;
+  }
 `;
 
 const EditorContainer = styled(TitleContainer)`
-  min-width: fit-content;
   width: 1024px;
   height: 500px;
   margin-bottom: 1rem;
+  overflow: hidden;
+  .toastui-editor-main {
+    :has(.ProseMirror-focused) {
+      border-radius: 3px;
+      border: 1px solid rgba(0, 103, 194, 0.4);
+      box-shadow: 0 0 0 4px rgba(144, 203, 255, 0.4);
+    }
+  }
+  @media screen and (max-width: 1024px) {
+    width: 100%;
+  }
+  @media screen and (max-width: 640px) {
+    .toastui-editor-defaultUI {
+      width: calc(100vw - 6rem);
+    }
+  }
 `;
 
 const TagsWrapper = styled(TitleContainer)`
@@ -397,6 +427,10 @@ const ButtonContainer = styled(TagsInputContainer)`
   justify-content: center;
   border: none;
   padding-bottom: 5rem;
+  &:focus-within {
+    border: none;
+    box-shadow: none;
+  }
 `;
 
 const SubmitButton = styled.button`
